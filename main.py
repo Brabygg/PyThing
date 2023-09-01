b6752e1156b837bc229ac3be3b502eaeda14312f
Nils Konradsson <niko0416@storsjogymnasiet.se>
2023-09-01 14:44:34 +0200

    Made a guessing game

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from random import randint
 
-res = input("Pick a random number from 1 - 65 536: ")
-if (int(res) == randint(1, 65536)):
-    print("Somehow, you were actually correct! Congrats!")
-else:
-    print("WRONG!")
-input()
+rand_num = randint(1, 10000)
+correct = False
+
+res = input("Pick a random number from 1 - 10000. You get ten guesses: ")
+for i in range(9):
+    res = int(res)
+    if (res == rand_num):
+        print("Somehow, you were actually correct! Congrats.")
+        correct = True
+        input()
+        break
+    else:
+        print(f"WRONG! {9 - i} guess{ '' if 9 - i == 1 else 'es' } left.")
+        print(f"(Hint: the number you're looking for is {'HIGHER' if rand_num > res else 'LOWER'})")
+        res = input("Continue: ")
+
+if (correct == False):
+    print(f"You are a fucking pathetic loser. Can't even get a 1/1000 chance to guess {rand_num}? Get out of my sight, maggot.")
+    input()
