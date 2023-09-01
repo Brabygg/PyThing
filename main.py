ccbad0f4d69836a96643bfd2ba903be2b93890f5
Nils Konradsson <niko0416@storsjogymnasiet.se>
2023-09-01 13:04:30 +0200

    Did stuff

---
+++ main.py
@@ -0,0 +1,8 @@
+from random import randint
+
+var = "\nBalls"
+
+print("Hello world!" + var)
+
+for i in range(1000):
+    print(f"On a scale from one to ten, I rate Python programming {randint(1, 10)}")
